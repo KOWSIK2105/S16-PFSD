d4251dd1951fdc02df974dd2e171396934fe87c0
BSSN KOWSIK <bssnkowsik123@gmail.com>
2022-08-20 14:59:41 +0530

    created  a new commit

---
+++ os module.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    import os
+    print(os.name)    # This function provides the name of the operating system module that it imports.
+                     # Currently, it registers 'posix', 'nt', 'os2', 'ce', 'java' and 'riscos'.
+
