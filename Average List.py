51258dbc87e2304e7a1a9dcfd2a0340d99081d4b
BSSN KOWSIK <bssnkowsik123@gmail.com>
2022-09-02 23:33:08 +0530

    AVERAGE OF NUMBERS

---
+++ Average List.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    List = [8,91,6,4,2,7,3,1,5]
+    sumofList=sum(List)    # adding all the numbers in List
+    length=len(List)       # finding the length of List
+    ListAvg = sumofList/length  # average = sumoflist/length
+    print("Average of numbers : ",ListAvg) #printing average
